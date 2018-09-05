f492ce2b27df4d57ecd1bec08911ed4afaf93dd4
Sarah Stolle <sstolle@xccelerated.io>
2018-09-05 17:02:08 +0200

    made it actually work on command line

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# roman-sarah
+## 1000 ways to Rome and back
 
+This little commandline app that can convert positive arabic numbers into roman numerals.
+
+For example:
+1. `>>> python roman_mapping.py 42` outputs **XVII**
+2. `>>> python roman_mapping.py MCMXLVIII` outputs **1948**
+
+Going to Rome one step at a time ... 
--- roman_mapping.py
+++ roman_mapping.py
@@ -1,6 +1,12 @@
+import sys
 
+def num2roman(n:int, roman_val=''):
+    """Converts a positive arabic number into a roman numeral. #numberArt
 
-def num2roman(n, roman_val=''):
+    :param n: a positive integer you're interested in
+    :param roman_val: just don't bother
+    :return: a string representing the roman numeral of n
+    """
     mapping = {
         1: 'I',
         4: 'IV',
@@ -19,6 +25,8 @@ def num2roman(n, roman_val=''):
 
     if n == 0:
         return roman_val
+    elif n < 0:
+        raise ValueError("Romans didn't have negative numbers.")
 
     for key, value in sorted(mapping.items(), reverse=True):
         if n >= key:
@@ -28,7 +36,12 @@ def num2roman(n, roman_val=''):
 
 
 def roman2num(s:str, n:int = 0):
-    """ Translate roman numbers into numeric"""
+    """Translate roman numbers into numeric
+
+    :param s: a string with a roman numeral you want to convert
+    :param n: don't bother plz
+    :return: the arabic number of your roman numeral
+    """
     mapping = {
         'I': 1,
         'IV': 4,
@@ -47,14 +60,21 @@ def roman2num(s:str, n:int = 0):
     print(f"roman = {s}, n = {n}")
     if s == "":
         return n
+    s = s.upper()
 
     for key, value in sorted(mapping.items(), key= lambda x: x[1], reverse=True):
         if s.startswith(key):
             n += value
             s = s.replace(key, "", 1)
             return roman2num(s, n)
+    # if the letter is not in the dictionary raise an error
+    raise ValueError("You didn't provide a correct input.")
 
 
 if __name__ == "__main__":
-    print(num2roman(2018))
-    print(roman2num("MMXVIII"))
+    to_be_translated = sys.argv[1]
+    try:
+        to_be_translated = int(to_be_translated)
+        num2roman(to_be_translated)
+    except ValueError:
+        roman2num(to_be_translated)
